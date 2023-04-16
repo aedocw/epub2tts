479fcfe94d1af61a0e81ba6a740e5a3a6fe93039
Christopher Aedo <doc@aedo.net>
2023-04-16 12:39:04 -0700

    Added more info to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,13 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='epub-to-TTS-app',
-    version='1.0.3',
+    name='epub2tts',
+    description='Tool to read an epub to mp3 using TTS',
+    author='Christopher Aedo',
+    author_email='doc@aedo.net',
+    url='https://github.com/aedocw/epub2tts',
+    license='Apache License, Version 2.0',
+    version='1.0.4',
     packages=find_packages(),
     install_requires=requirements,
     py_modules=['epub2tts'],
