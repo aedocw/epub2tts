19e68c73a46c00f5cd957c7ddd15391249b3b971
Christopher Aedo <doc@aedo.net>
2023-12-03 15:56:21 -0800

    Major refactor, classes and XTTS streaming

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open('requirements.txt') as f:
 
 setup(
     name='epub2tts',
-    description='Tool to read an epub to mp3 using TTS',
-    author='Christopher Aedo',
+    description='Tool to read an epub to audiobook using AI TTS',
+    author='Christopher Aedo linkedin.com/in/aedo',
     author_email='doc@aedo.net',
     url='https://github.com/aedocw/epub2tts',
     license='Apache License, Version 2.0',
-    version='1.5.7',
+    version='2.0.0',
     packages=find_packages(),
     install_requires=requirements,
     py_modules=['epub2tts'],
