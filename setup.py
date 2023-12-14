a1656cc81c24427982d96f787cd733d7998e6518
Francesco Manghi <wonka929@gmail.com>
2023-12-14 14:02:56 +0100

    updated version

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='epub2tts',
-    description='Tool to read an epub to audiobook using AI TTS',
-    author='Christopher Aedo linkedin.com/in/aedo',
-    author_email='doc@aedo.net',
-    url='https://github.com/aedocw/epub2tts',
-    license='Apache License, Version 2.0',
-    version='2.1.8',
+    name="epub2tts",
+    description="Tool to read an epub to audiobook using AI TTS",
+    author="Christopher Aedo linkedin.com/in/aedo",
+    author_email="doc@aedo.net",
+    url="https://github.com/aedocw/epub2tts",
+    license="Apache License, Version 2.0",
+    version="2.1.9",
     packages=find_packages(),
     install_requires=requirements,
-    py_modules=['epub2tts'],
-    entry_points={
-        'console_scripts': [
-            'epub2tts = epub2tts:main'
-        ]
-    }
+    py_modules=["epub2tts"],
+    entry_points={"console_scripts": ["epub2tts = epub2tts:main"]},
 )
