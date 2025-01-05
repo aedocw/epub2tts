a7e1e6ee4f42bfb040a6ee50d1aaa4c9e4c0a9c2
Christopher Aedo <doc@aedo.net>
2025-01-05 07:30:23 -0800

    Updated install steps for making a new model

--- utils/install_reqs.sh
+++ utils/install_reqs.sh
@@ -1,7 +1,9 @@
 #NOTE, this should probably be done in a virtual environment
 #python -m venv .venv; source .venv/bin/activate
 rm -rf TTS/ # delete repo to be able to reinstall if needed
-git clone --branch xtts_demo https://github.com/coqui-ai/TTS.git
+#git clone --branch xtts_demo https://github.com/coqui-ai/TTS.git
+#Use this repo instead
+git clone https://github.com/idiap/coqui-ai-TTS TTS
 pip install --use-deprecated=legacy-resolver -e TTS
 pip install --use-deprecated=legacy-resolver -r TTS/TTS/demos/xtts_ft_demo/requirements.txt
 pip install typing_extensions==4.8 numpy==1.26.2
